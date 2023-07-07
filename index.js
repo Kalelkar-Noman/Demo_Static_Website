4f01d7ec152eed6f67a076ea454f3eefeb11f65e
Kalelkar-Noman <kalelkarnoman014@gmail.com>
2023-07-07 18:07:21 +0530

    updated

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-/*
- * Toggles the visibility of the drop-down menu.
- */
+"use strict";
 function toggle() {
-  const drop = document.querySelector("#drop");
-  const check = document.querySelector("#check");
-  drop.classList.toggle("open", !drop.classList.contains("open"));
-  check.innerHTML = drop.classList.contains("open") ? "close" : "menu";
+  let e = document.querySelector("#drop"),
+    o = document.querySelector("#check");
+  e.classList.toggle("open", !e.classList.contains("open")),
+    (o.innerHTML = e.classList.contains("open") ? "close" : "menu");
 }
-document.querySelector("#drop").addEventListener("click", toggle);
-/**
- * Changes the background color of the element to white.
- */
-function hoverdiv(elementId) {
-  document.getElementById(elementId).style.backgroundColor = "white";
+function hoverdiv(e) {
+  document.getElementById(e).style.backgroundColor = "white";
 }
-/**
- * Changes the background color of the element to the specified color.
- */
-function nohoverdiv(elementId, divcolor) {
-  document.getElementById(elementId).style.backgroundColor = divcolor;
+function nohoverdiv(e, o) {
+  document.getElementById(e).style.backgroundColor = o;
 }
-/**
- * Shows the "Back to top" button when the user scrolls down.
- */
 function scrollFunction() {
-  const mybutton = document.querySelector("#myBtn");
-  mybutton.style.display = window.scrollY > 20 ? "block" : "none";
+  let e = document.querySelector("#myBtn");
+  e.style.display = window.scrollY > 20 ? "block" : "none";
 }
-document.addEventListener("scroll", scrollFunction);
-
 function topFunction() {
   window.scrollTo(0, 0);
 }
-
-const observer = new IntersectionObserver(entries => entries.filter(entry => entry.intersectionRatio > 0).forEach(entry => entry.target.classList.add("inview")));
-const elements = document.querySelectorAll(".fade-up, .zoom-out");
-elements.forEach((element) => {
-  observer.observe(element);
+document.querySelector("#drop").addEventListener("click", toggle),
+  document.addEventListener("scroll", scrollFunction);
+const observer = new IntersectionObserver((e) =>
+    e
+      .filter((e) => e.intersectionRatio > 0)
+      .forEach((e) => e.target.classList.add("inview"))
+  ),
+  elements = document.querySelectorAll(".fade-up, .zoom-out");
+elements.forEach((e) => {
+  observer.observe(e);
 });
